0d76d6fcdb168979f46e373255910a743e91ecb4
szhokee <szhokee@gmail.com>
2023-04-02 22:40:01 +0600

    v3

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
-    path('', include('chat.urls'))
+    path('chat/', include('chat.urls'))
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
