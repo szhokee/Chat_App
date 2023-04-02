1709e85e8acdcdf112e42c3fc97aa57cfaf95938
szhokee <szhokee@gmail.com>
2023-04-02 22:38:52 +0600

    v2

--- chat/models.py
+++ chat/models.py
@@ -9,6 +9,9 @@ class Thread(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'Thread {self.id}'
+
 
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -16,3 +19,6 @@ class Message(models.Model):
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE, related_name='messages')
     created = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Message {self.id}'
