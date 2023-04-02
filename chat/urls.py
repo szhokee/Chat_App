966dfae13408be8a71b699658dafbfacf640dea8
szhokee <szhokee@gmail.com>
2023-04-02 23:55:20 +0600

    v4

--- chat/urls.py
+++ chat/urls.py
@@ -11,3 +11,10 @@ urlpatterns = [
     path('api/set_message_status', SetMessageStatus.as_view(), name='set_message_status'),
     re_path(r'^api/count_unread_message/(?P<user_id>\d+)$', CountUnreadMessage.as_view(), name='count_unread_message'),
 ]
+
+
+# api/create_delete_chat/<int:user1_id>/<int:user2_id> - создание или удаление чата между пользователями с идентификаторами user1_id и user2_id
+# api/get_threads/<int:user_id> - получение списка чатов пользователя с идентификатором user_id
+# api/get_create_message/<int:thread_id> - получение и создание сообщений в чате с идентификатором thread_id
+# api/set_message_status - изменение статуса сообщения в чате
+# api/count_unread_message/<int:user_id> - получение количества непрочитанных сообщений пользователя с идентификатором user_id
